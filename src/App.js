d8c67007a7ec62bd30f62601581c09561eac7160
Jalec <jordialej14@gmail.com>
2023-05-22 16:32:20 +0200

    css jump button

--- src/App.js
+++ src/App.js
@@ -5,9 +5,28 @@ import { Pages } from './pages/Pages.jsx';
 import { Skills } from './pages/Skills.jsx';
 import { Contact } from './pages/ContactMe.jsx';
 import { Footer } from './components/footer';
-
+import { FaArrowUp } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 200) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+
   return (
     <div>
       <Landing />
@@ -16,6 +35,9 @@ function App() {
       <Skills />
       <Contact />
       <Footer />
+      <button className={`jump-btn ${showScrollButton ? 'active' : ''}`} onClick={scrollToTop}>
+        <FaArrowUp size={20} color='white' className='arrow-icon'/>
+      </button>
     </div>
   );
 }
